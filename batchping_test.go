aef69151881f3e5be3f59c79bfcec26780cfd0bf
Rosenlo <rosenluov@gmail.com>
2021-01-08 16:47:44 +0800

    add unittest for timeout retry

--- batchping_test.go
+++ batchping_test.go
@@ -1,8 +1,10 @@
 package batchping
 
 import (
+	"log"
 	"runtime/debug"
 	"testing"
+	"time"
 )
 
 func AssertNoError(t *testing.T, err error) {
@@ -48,6 +50,61 @@ func TestMutilAddrs(t *testing.T) {
 
 		err = bp.Run(tests[i].addrs)
 		AssertNoError(t, err)
+	}
+}
+
+func TestTimeoutRetry(t *testing.T) {
+	type input struct {
+		privileged bool
+		addrs      []string
+	}
+	tests := []input{
+		{
+			false,
+			[]string{
+				"127.0.0.1",
+				"127.0.0.2", // not exist
+			},
+		},
+	}
+	for i := 0; i < len(tests); i++ {
+		var addrs []string
+		bp, err := New(tests[i].privileged)
+		AssertNoError(t, err)
+
+		addrs = tests[i].addrs
+
+		bp.Count = 3
+		bp.Timeout = time.Second * 5
+		bp.OnFinish = func(mapStat map[string]*Statistics) {
+			addrs = []string{}
+			for _, stat := range mapStat {
+				t.Log(stat)
+				if stat.PacketsRecv != stat.PacketsSent {
+					addrs = append(addrs, stat.Addr)
+				}
+			}
+		}
+
+		retry := 3
+		err = bp.Run(addrs)
+		if err == nil {
+			return
+		}
+
+		log.Printf("[error] %v", err)
+
+		for i := 0; i < retry; i++ {
+			log.Printf("[debug] retry: %v", addrs)
+			if len(addrs) != 0 {
+				err := bp.Run(addrs)
+				if err == nil {
+					break
+				}
+				t.Logf("[error] %v", err)
+			}
+		}
+
 	}
 }
 
